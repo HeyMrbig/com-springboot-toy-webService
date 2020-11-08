7ec9f56b7e12f41b52c0495ab7b7db43a443d934
Ican <cs2772@naver.com>
2020-11-08 16:35:44 +0900

    OAuth2 설정

--- src/main/java/com/springboot/toy/webService/web/dto/IndexController.java
+++ src/main/java/com/springboot/toy/webService/web/dto/IndexController.java
@@ -1,5 +1,7 @@
 package com.springboot.toy.webService.web.dto;
 
+import com.springboot.toy.webService.config.auth.LoginUser;
+import com.springboot.toy.webService.config.auth.dto.SessionUser;
 import com.springboot.toy.webService.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
